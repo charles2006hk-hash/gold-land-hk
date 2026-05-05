4af602a097eb9f7af20b0bc9de84ee7cab5200d1
charles2006hk-hash <charles2006hk@gmail.com>
2026-05-05 22:26:03 +0800

    Update import path for firebase in vip-login page

--- src/app/vip-login/page.js
+++ src/app/vip-login/page.js
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
-import { db } from '@/lib/firebase';
+import { db } from '../../lib/firebase';
 import { collection, query, where, getDocs, addDoc } from 'firebase/firestore';
 
 export default function VipLogin() {
